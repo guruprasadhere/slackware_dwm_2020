8f07eb574963f4fb1665c5aa9b0f5f9a982558c4
Guruprasad <34624298+guruprasadhere@users.noreply.github.com>
2020-04-02 19:50:13 +0530

    Version2.0

--- config.h
+++ config.h
@@ -1,25 +1,23 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
+static const char *fonts[] = {
+	"Liberation Mono:size=9"
+};
+static const char dmenufont[]       = "monospace:size=10";
+static const char normbordercolor[] = "#444444";
+static const char normbgcolor[]     = "#222222";
+static const char normfgcolor[]     = "#dedede";
+static const char selbordercolor[]  = "#005577";
+static const char selbgcolor[]      = "#005577";
+static const char selfgcolor[]      = "#eeeeee";
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 0;        /* 0 means bottom bar */
-static const char *fonts[]          = { "DejaVu Sans mono:size=8" };
-static const char dmenufont[]       = "Terminus:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#FF9200";
-static const char col_gray4[]       = "#FF9200";
-static const char col_cyan[]        = "#004D00";/*   5577 */
-static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
-};
+static const int topbar             = 1;        /* 0 means bottom bar */
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4" ,"5","6"};
+static const char *tags[] = { "1", "2", "3", "4", "5", "6" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -32,9 +30,9 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static const float mfact     = 0.5; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
@@ -42,7 +40,6 @@ static const Layout layouts[] = {
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
 };
-
 /* key definitions */
 #define MODKEY Mod1Mask
 #define TAGKEYS(KEY,TAG) \
@@ -56,9 +53,9 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *browsercmd[] = { "surf", NULL };
+static const char *browsercmd[] = { "luakit", NULL };
 static const char *browsercmdg[]={"brave-browser-stable",NULL};
 static const char *fmcmd[] = { "st", "-e", "nnn" };
 static const char *htopcmd[] = { "st", "-e", "htop", NULL };
@@ -68,6 +65,7 @@ static const char *voldown[] = { "amixer", "set", "Master", "5%-", NULL };
 static const char *brtup[] = { "xbacklight", "-inc","5", NULL };
 static const char *brtdown[] = { "xbacklight", "-dec","5", NULL };
 static const char *texstudio[] = { "texmaker",NULL };
+static const char *lock[] = { "st", "-e", "slock" };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -96,6 +94,7 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
 	{ MODKEY,                       XK_i,            spawn, {.v = browsercmd } },
 	{ MODKEY|ShiftMask,             XK_i,            spawn, {.v = browsercmdg } },
+	{ MODKEY|ShiftMask,             XK_x,            spawn, {.v = lock } },
 	{ MODKEY,                       XK_p,            spawn, {.v = fmcmd } },
         { MODKEY,                       XK_o,            spawn, {.v = htopcmd } },
         { MODKEY,                       XK_c,            spawn, {.v = mocpcmd } },
@@ -129,4 +128,5 @@ static Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
+
 
